fe64069f6781aaf294da9fc392c7ec858f892fae
Administrator <Administrator@USER-20180628IN>
2020-01-03 12:51:27 +0800

    v3

--- test6666/src/test6666/test.java
+++ test6666/src/test6666/test.java
@@ -4,7 +4,8 @@ public class test {
 	public static void main(String[] args) {
 		String s ="ddd";
 		int a =10;
-		
+		boolean c =true;
+		String u ="other";
 		System.out.println(s);
 		System.out.println(a+s);
 		System.out.println(a);
